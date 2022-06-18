1e94f41ad7e462c03a669f808ca9f55df61523c3
Jose Sarmiento <jose.dev.official@gmail.com>
2022-06-18 20:12:04 +0800

    Attempt to fix unexpected token in client

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use('/api/v1/notifications', notifications);
 
 // serving static assets from react client
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
   });
